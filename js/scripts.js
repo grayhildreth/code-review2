4d038baf86a4500a612437f50deb5d62fb3b2934
Gray Hildreth <grayhildreth@gmail.com>
2016-06-12 13:02:13 -0700

    adds more branching to the JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,11 @@ $(document).ready(function() {
     var footwear = $("#shoes-sandals").val();
     var transportation = $("#transportation").val();
 
-    if (destination === 'beach' && music === 'reggae' && transportation === 'airplane') {
+    if ((destination === 'beach' && music === 'reggae' && transportation === 'airplane' || destination === 'beach' && music === 'reggae' && weather === 'hot' || destination === 'beach' && music === 'reggae' && footwear === 'sandals')) {
       result = ("Go to Jamaica");
-    } else if (destination === 'mountains' && music === 'country' && transportation === 'car') {
+    } else if ((destination === 'mountains' && music === 'country' && transportation === 'car' || destination === 'mountains' && music === 'country' && footwear === 'shoes')) {
       result = ("Go to Bend, Oregon!");
-    } else if (destination === 'beach' && music === 'country' && transportation === 'car') {
+    } else if ((destination === 'beach' && music === 'country' && transportation === 'car' || destination === 'beach' && weather === 'hot' && transportation === 'car' || destination === 'beach' && music === 'reggae' && transportation === 'car' || destination === 'beach' && footwear === 'sandals' && transportation === 'car' )) {
       result = ("Go to Cannon Beach, Oregon!");
     } else {
         alert ('Inconclusive, try other answers...');
